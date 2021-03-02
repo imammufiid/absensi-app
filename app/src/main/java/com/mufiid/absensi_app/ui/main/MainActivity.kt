9c7ff4d2d658660e38295fa62ec93f837b06b725
imufiid <imufiid98@gmail.com>
2021-03-02 10:35:19 +0700

    idk

--- app/src/main/java/com/mufiid/absensi_app/ui/main/MainActivity.kt
+++ app/src/main/java/com/mufiid/absensi_app/ui/main/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mufiid.absensi_app.R
 
@@ -19,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        // val appBarConfiguration = AppBarConfiguration(setOf(
-        //         R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        // setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
