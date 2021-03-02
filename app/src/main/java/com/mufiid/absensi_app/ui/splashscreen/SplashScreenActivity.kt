ed8941f9387ae306fe1302ebfd9982bf3bfd0651
imufiid <imufiid98@gmail.com>
2021-03-02 08:10:22 +0700

    create SplashScreenActivity.kt

---
+++ app/src/main/java/com/mufiid/absensi_app/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.mufiid.absensi_app.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mufiid.absensi_app.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
