312b5632120fa2de4309ba93d79844af8015c0f4
imufiid <imufiid98@gmail.com>
2021-03-02 08:09:48 +0700

    move directory of main activity

--- app/src/main/java/com/mufiid/absensi_app/ui/main/MainActivity.kt
+++ app/src/main/java/com/mufiid/absensi_app/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mufiid.absensi_app
+package com.mufiid.absensi_app.ui.main
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.mufiid.absensi_app.R
 
 class MainActivity : AppCompatActivity() {
 
