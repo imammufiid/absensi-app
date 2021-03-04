0d2bf1e1facc520bbcd4f6689069bdf93bd28b2a
imufiid <imufiid98@gmail.com>
2021-03-04 12:14:58 +0700

    created ui edit profile

--- app/src/main/java/com/mufiid/absensi_app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mufiid/absensi_app/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.mufiid.absensi_app.ui.profile
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,26 +8,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mufiid.absensi_app.R
+import com.mufiid.absensi_app.databinding.ProfileFragmentBinding
+import com.mufiid.absensi_app.ui.profileedit.EditProfileActivity
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), View.OnClickListener {
 
     companion object {
         fun newInstance() = ProfileFragment()
     }
 
+    private lateinit var _bind: ProfileFragmentBinding
     private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+        _bind = ProfileFragmentBinding.inflate(layoutInflater, container, false)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        return _bind.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        _bind.btnEditProfile.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_edit_profile -> startActivity(Intent(context, EditProfileActivity::class.java))
+        }
     }
 
 }
