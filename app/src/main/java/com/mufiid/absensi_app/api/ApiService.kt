925a5eb9fa4f3084129a9bbd43fcaa2be48e6e3d
imufiid <imufiid98@gmail.com>
2021-03-06 08:56:47 +0700

    add some service api

--- app/src/main/java/com/mufiid/absensi_app/api/ApiService.kt
+++ app/src/main/java/com/mufiid/absensi_app/api/ApiService.kt
@@ -1,4 +1,53 @@
 package com.mufiid.absensi_app.api
 
+import com.mufiid.absensi_app.data.source.local.entity.AttendanceEntity
+import com.mufiid.absensi_app.data.source.local.entity.TaskEntity
+import com.mufiid.absensi_app.data.source.local.entity.UserEntity
+import com.mufiid.absensi_app.data.source.remote.response.WrappedResponse
+import retrofit2.http.*
+
 interface ApiService {
+
+    // AUTH ---------------------------------------------------
+
+    // LOGIN
+    @FormUrlEncoded
+    @POST("auth/login")
+    suspend fun login(
+        @Field("email") email: String?,
+        @Field("password") password: String?
+    ): WrappedResponse<UserEntity>
+
+    // REGISTRATION
+    @FormUrlEncoded
+    @POST("auth/register")
+    suspend fun registerUser(
+        @Field("name") name: String?,
+        @Field("nik") nik: String?,
+        @Field("email") email: String?,
+        @Field("password") password: String?
+    ): WrappedResponse<UserEntity>
+
+    // LOGOUT
+    @FormUrlEncoded
+    @POST("auth/logout")
+    suspend fun logout(
+        @Header("Authorization") token: String?
+    ): WrappedResponse<UserEntity>
+
+    // ATTENDANCE ---------------------------------------------------
+    // SHOW ATTENDANCE
+    @GET("attendance/show")
+    suspend fun showAttendance(
+        @Query("id_employee") idUser: Int?
+    ): WrappedResponse<AttendanceEntity>
+
+    // TASK ---------------------------------------------------
+    // SHOW ALL TASK
+    @GET("task")
+    suspend fun showAllTask(
+        @Query("user_id") idUser: Int?,
+        @Query("date") date: String?,
+        @Query("is_admin") isAdmin: Int?
+    ): WrappedResponse<TaskEntity>
 }
