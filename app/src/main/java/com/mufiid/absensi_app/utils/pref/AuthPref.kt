6ad56cf1f6de80f9889f1fc3b8b1cffe1de90d3f
imufiid <imufiid98@gmail.com>
2021-03-06 08:59:15 +0700

    create shared pref

---
+++ app/src/main/java/com/mufiid/absensi_app/utils/pref/AuthPref.kt
@@ -0,0 +1,18 @@
+package com.mufiid.absensi_app.utils.pref
+
+import android.content.Context
+import androidx.core.content.edit
+
+object AuthPref {
+    fun isLoggedIn(context: Context): Boolean {
+        val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
+        return pref.getBoolean("ISLOGGEDIN", false)
+    }
+
+    fun setIsLoggedIn(context: Context, state: Boolean) {
+        val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
+        pref.edit {
+            putBoolean("ISLOGGEDIN", state)
+        }
+    }
+}
---
+++ app/src/main/java/com/mufiid/absensi_app/utils/pref/UserPref.kt
@@ -0,0 +1,25 @@
+package com.mufiid.absensi_app.utils.pref
+
+import android.content.Context
+import androidx.core.content.edit
+import com.mufiid.absensi_app.data.source.local.entity.UserEntity
+
+object UserPref {
+    fun getUserData(context: Context): UserEntity? {
+        val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
+        return UserEntity().apply {
+            email = pref.getString("EMAIL", "")
+            name = pref.getString("NAME", "")
+            id = pref.getInt("ID_USER", 0)
+        }
+    }
+
+    fun setUserData(context: Context, user: UserEntity?) {
+        val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
+        pref.edit {
+            putString("EMAIL", user?.email)
+            putString("NAME", user?.name)
+            user?.id?.let { putInt("ID_USER", it) }
+        }
+    }
+}
