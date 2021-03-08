217afe416abc6655e44f96b919980decf2e914a4
imufiid <imufiid98@gmail.com>
2021-03-08 08:40:16 +0700

    setting up recycle view to task from api

--- app/src/main/java/com/mufiid/absensi_app/ui/task/TaskFragment.kt
+++ app/src/main/java/com/mufiid/absensi_app/ui/task/TaskFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mufiid.absensi_app.R
 import com.mufiid.absensi_app.databinding.FragmentTaskBinding
 import com.mufiid.absensi_app.ui.addtask.AddTaskActivity
+import com.mufiid.absensi_app.utils.pref.UserPref
 import com.mufiid.absensi_app.viewmodel.ViewModelFactory
 
 class TaskFragment : Fragment(), View.OnClickListener {
     private lateinit var _bind : FragmentTaskBinding
     private lateinit var taskViewModel: TaskViewModel
+    private lateinit var taskAdapter: TaskAdapter
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -33,15 +37,62 @@ class TaskFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        taskViewModel.text.observe(viewLifecycleOwner, {
-
-        })
 
         init()
+        // observe view model
+        observerViewModel()
+    }
+
+    private fun observerViewModel() {
+        // loading
+        taskViewModel.loading.observe(viewLifecycleOwner, {
+            if (it) {
+                _bind.progressBar.visibility = View.VISIBLE
+            } else {
+                _bind.progressBar.visibility = View.GONE
+            }
+        })
+        // message
+        taskViewModel.message.observe(viewLifecycleOwner, {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        })
+
+        // get all task
+        taskViewModel.taskData.observe(viewLifecycleOwner, {
+            if (!it.isNullOrEmpty()) {
+                taskAdapter.apply {
+                    addTask(it)
+                }
+            } else {
+                Toast.makeText(context, "NULL", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun init() {
+        taskAdapter = TaskAdapter()
+        setRecyclerView()
+        setViewModel()
+
         _bind.btnAdd.setOnClickListener(this)
+    }
+
+    private fun setViewModel() {
+        // get all task
+        val userPref = context?.let { UserPref.getUserData(it) }
+        userPref?.token?.let { token ->
+            taskViewModel.getTaskData(
+                token, userPref.id, null, null
+            )
+        }
+    }
+
+    private fun setRecyclerView() {
+        with(_bind.rvTask) {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = taskAdapter
+        }
     }
 
     override fun onClick(v: View?) {
