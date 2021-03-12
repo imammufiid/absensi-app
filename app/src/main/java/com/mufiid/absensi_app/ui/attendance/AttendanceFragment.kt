61a48d3015604a2d27a21410ebe2cc18477b7f66
imufiid <imufiid98@gmail.com>
2021-03-12 09:52:45 +0700

    prepare observe view model and add view binding in this fragment

--- app/src/main/java/com/mufiid/absensi_app/ui/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/mufiid/absensi_app/ui/attendance/AttendanceFragment.kt
@@ -9,10 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.mufiid.absensi_app.R
+import com.mufiid.absensi_app.databinding.FragmentAttendanceBinding
 import com.mufiid.absensi_app.viewmodel.ViewModelFactory
 
 class AttendanceFragment : Fragment() {
-
+    private lateinit var _bind: FragmentAttendanceBinding
     private lateinit var attendanceViewModel: AttendanceViewModel
 
     override fun onCreateView(
@@ -20,13 +21,26 @@ class AttendanceFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        _bind = FragmentAttendanceBinding.inflate(layoutInflater, container, false)
         val factory = ViewModelFactory.getInstance(requireActivity())
         attendanceViewModel =
                 ViewModelProvider(this, factory).get(AttendanceViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_attendance, container, false)
-        attendanceViewModel.text.observe(viewLifecycleOwner, {
+        return _bind.root
+    }
 
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+        observeViewModel()
+    }
+
+    private fun init() {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun observeViewModel() {
+        TODO("Not yet implemented")
     }
 }
