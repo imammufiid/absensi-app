6adf9b667a56c6af6153cbbf991f6292da9bfd2a
imufiid <imufiid98@gmail.com>
2021-04-02 09:42:02 +0700

    add : permission checker

--- app/src/main/java/com/mufiid/absensi_app/ui/main/MainActivity.kt
+++ app/src/main/java/com/mufiid/absensi_app/ui/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.mufiid.absensi_app.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mufiid.absensi_app.R
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +20,23 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
+        permissionChecker()
+    }
+
+    private fun permissionChecker() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA),
+                    1
+                )
+            }
+        }
     }
 }
