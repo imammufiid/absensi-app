adba064131cce351285f0c12ea53085d80cff1d0
imufiid <imufiid98@gmail.com>
2021-04-09 10:43:19 +0700

    fix : import kotlin parcelize

--- app/src/main/java/com/mufiid/absensi_app/data/source/local/entity/TaskEntity.kt
+++ app/src/main/java/com/mufiid/absensi_app/data/source/local/entity/TaskEntity.kt
@@ -2,7 +2,7 @@ package com.mufiid.absensi_app.data.source.local.entity
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class TaskEntity(
