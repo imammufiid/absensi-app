30332bdc440108a51d8d0e77362ccaeea35f8586
imufiid <imufiid98@gmail.com>
2021-05-06 21:16:47 +0700

    update : refactor code

--- app/src/main/java/com/skripsi/absensi_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/skripsi/absensi_app/ui/home/HomeFragment.kt
@@ -18,6 +18,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -45,27 +46,25 @@ import java.util.*
 
 class HomeFragment : Fragment() {
     private lateinit var _bind: FragmentHomeBinding
-    private lateinit var homeViewModel: HomeViewModel
-    private lateinit var taskAdapter: TaskAdapter
     private var filePath: String? = null
     private var taskEntity: TaskEntity? = null
     private var part: MultipartBody.Part? = null
+
+    private val homeViewModel: HomeViewModel by viewModels {
+        ViewModelFactory.getInstance(requireActivity())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _bind = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return _bind.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val factory = ViewModelFactory.getInstance(requireActivity())
-        homeViewModel = ViewModelProvider(this, factory)
-            .get(HomeViewModel::class.java)
 
         init()
         // observe view model
@@ -74,53 +73,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun init() {
-//        taskAdapter = TaskAdapter { item ->
-//            taskEntity = item
-//
-//            BottomSheetUploadFileTask().show(
-//                childFragmentManager,
-//                BottomSheetUploadFileTask.TAG
-//            )
-//        }
-        taskAdapter = TaskAdapter(object : TaskAdapter.CheckListTask {
-            override fun showUploadFile(item: TaskEntity?) {
-                taskEntity = item
-
-                BottomSheetUploadFileTask().show(
-                    childFragmentManager,
-                    BottomSheetUploadFileTask.TAG
-                )
-            }
-
-            override fun downloadFile(filePath: String?) {
-                val path = URI(filePath).path
-                val nameFile = path.substring(path.lastIndexOf("/") + 1)
-                val request = DownloadManager.Request(Uri.parse(filePath)).apply {
-                    setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-                    setTitle("Donwload file tugas")
-                    setDescription("Deskripsi file tugas")
-                    setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                    setDestinationInExternalPublicDir(
-                        Environment.DIRECTORY_DOWNLOADS, "${System.currentTimeMillis()}_${nameFile}"
-                    )
-                }
-
-                val manager = activity?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-                manager.enqueue(request)
-            }
-        })
-        // taskAdapter.notifyItemChanged(1)
-
-        setRecyclerView()
         setViewModel()
-
-        // set click listener
-        _bind.btnScan.setOnClickListener(
-            Navigation.createNavigateOnClickListener(
-                R.id.action_homeFragment_to_scannerFragment,
-                null
-            )
-        )
     }
 
     private fun setViewModel() {
@@ -131,25 +84,9 @@ class HomeFragment : Fragment() {
         // get all task
         val userPref = context?.let { UserPref.getUserData(it) }
         userPref?.token?.let { token ->
-            homeViewModel.getTaskData(
-                token, userPref.id, null, null
-            )
-
             homeViewModel.attendanceToday(
                 token, userPref.id
             )
-
-            homeViewModel.getMyPoint(
-                token, userPref.id
-            )
-        }
-    }
-
-    private fun setRecyclerView() {
-        with(_bind.rvTask) {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(context)
-            adapter = taskAdapter
         }
     }
 
@@ -171,9 +108,11 @@ class HomeFragment : Fragment() {
                             .toRequestBody("text/plain".toMediaTypeOrNull())
                     }
 
-                val file = File(filePath)
-                val reqFile = file.asRequestBody("*/*".toMediaTypeOrNull())
-                part = MultipartBody.Part.createFormData("file", file.name, reqFile)
+                if (filePath != null) {
+                    val file = File(filePath)
+                    val reqFile = file.asRequestBody("*/*".toMediaTypeOrNull())
+                    part = MultipartBody.Part.createFormData("file", file.name, reqFile)
+                }
 
                 homeViewModel.markCompleteTask(
                     headers, taskId, userId, part
@@ -236,8 +175,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun getDateTimeToday() {
-        var date: String = ""
-        date = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        var date = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val current = LocalDateTime.now()
             val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.getDefault())
             current.format(formatter)
@@ -245,7 +183,7 @@ class HomeFragment : Fragment() {
             Date().toString()
         }
 
-        _bind.dateToday.text = date
+        _bind.detailAttendance.dateToday.text = date
     }
 
     private fun observerViewModel() {
@@ -257,56 +195,36 @@ class HomeFragment : Fragment() {
         homeViewModel.loading.observe(viewLifecycleOwner, {
             Log.d("LD_LOAD", it.toString())
             if (it) {
-                _bind.progressBar.visibility = View.VISIBLE
+                // TODO : progressBar Visible
+                // _bind.progressBar.visibility = View.VISIBLE
             } else {
-                _bind.progressBar.visibility = View.GONE
+                // TODO : progressBar Gone
+                // _bind.progressBar.visibility = View.GONE
             }
         })
 
         homeViewModel.msgGetTaskData.observe(viewLifecycleOwner, {
-            Log.d("LD_MSG_TASK", it.toString())
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
         })
 
         homeViewModel.msgAttendanceToday.observe(viewLifecycleOwner, {
-            Log.d("LD_MSG_ATD", it.toString())
             if (it != null) {
                 Snackbar.make(_bind.root, it, Snackbar.LENGTH_SHORT).show()
             }
         })
 
         homeViewModel.msgPoint.observe(viewLifecycleOwner, {
-            Log.d("LD_MSG_POINT", it.toString())
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
         })
 
-        homeViewModel.taskData.observe(viewLifecycleOwner, {
-            Log.d("LD_TASK", it.toString())
-            if (!it.isNullOrEmpty()) {
-                taskAdapter.apply {
-                    addTask(it)
-                }
-            } else {
-                _bind.tvEmpty.visibility = View.VISIBLE
-                _bind.tvEmpty.text = getString(R.string.data_empty, "tugas hari ini")
-            }
-        })
-
         homeViewModel.attendanceToday.observe(viewLifecycleOwner, {
-            Log.d("LD_ATD", it.toString())
             if (it != null) {
-                _bind.timeIn.text =
+                _bind.detailAttendance.timeIn.text =
                     if (it.timeComes == "0") getString(R.string.time) else it.timeComes
-                _bind.timeOut.text =
+                _bind.detailAttendance.timeOut.text =
                     if (it.timeGohome == "0") getString(R.string.time) else it.timeGohome
             }
         })
 
-        homeViewModel.pointData.observe(viewLifecycleOwner, {
-            Log.d("LD_POINT", it.toString())
-            if (it != null) {
-                _bind.score.text = it
-            }
-        })
     }
 }
