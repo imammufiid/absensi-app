e2ea877774d44c829f1c5865aebaed1c62c72ff0
imufiid <imufiid98@gmail.com>
2021-05-07 13:03:14 +0700

    fix : return live data attendance

--- app/src/main/java/com/skripsi/absensi_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/skripsi/absensi_app/ui/home/HomeViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.skripsi.absensi_app.data.source.BaseRepository
 import com.skripsi.absensi_app.data.source.local.entity.AttendanceEntity
-import com.skripsi.absensi_app.data.source.local.entity.TaskEntity
 import com.skripsi.absensi_app.data.source.remote.response.StatusResponse
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
@@ -17,26 +16,18 @@ class HomeViewModel(private val repo: BaseRepository) : ViewModel() {
     private val _text = MutableLiveData<String>()
     val greeting: LiveData<String> = _text
 
-    private val _msgGetTaskData = MutableLiveData<String?>()
-    val msgGetTaskData: LiveData<String?> = _msgGetTaskData
+    private val _msgAttendance = MutableLiveData<String?>()
+    val msgAttendance: LiveData<String?> = _msgAttendance
 
     private val _msgAttendanceToday = MutableLiveData<String?>()
     val msgAttendanceToday: LiveData<String?> = _msgAttendanceToday
 
-    private val _msgPoint = MutableLiveData<String?>()
-    val msgPoint: LiveData<String?> = _msgPoint
-
     private val _loading = MutableLiveData<Boolean>()
     val loading: LiveData<Boolean> = _loading
-
-    private val _taskData = MutableLiveData<List<TaskEntity>?>()
-    val taskData: LiveData<List<TaskEntity>?> = _taskData
 
     private val _attendanceToday = MutableLiveData<AttendanceEntity?>()
     val attendanceToday: LiveData<AttendanceEntity?> = _attendanceToday
 
-    private val _pointData = MutableLiveData<String?>()
-    val pointData : LiveData<String?> = _pointData
 
     fun setGreeting(name: String?) {
         this._text.value = name
@@ -60,6 +51,36 @@ class HomeViewModel(private val repo: BaseRepository) : ViewModel() {
             } catch (throwable: Throwable) {
                 _loading.postValue(false)
                 _msgAttendanceToday.postValue(throwable.message)
+            }
+        }
+    }
+
+    fun attendanceCome(
+        token: HashMap<String, String>,
+        userId: RequestBody?,
+        qrCode: RequestBody?,
+        latitude: RequestBody?,
+        longitude: RequestBody?,
+        attendanceType: RequestBody?,
+        information: RequestBody?,
+        fileInformation: MultipartBody.Part? = null
+    ) {
+        viewModelScope.launch {
+            try {
+                _loading.postValue(true)
+                val data = repo.attendanceScan(token, userId, qrCode, latitude, longitude, attendanceType, information, fileInformation)
+                when(data.value?.status) {
+                    StatusResponse.SUCCESS -> {
+                        _msgAttendance.postValue(data.value?.message)
+                        _attendanceToday.postValue(data.value?.body)
+                    }
+                    StatusResponse.EMPTY -> _msgAttendance.postValue(data.value?.message)
+                    else -> _msgAttendance.postValue(data.value?.message)
+                }
+                _loading.postValue(false)
+            } catch (throwable: Throwable) {
+                _msgAttendance.postValue(throwable.message)
+                _loading.postValue(false)
             }
         }
     }
