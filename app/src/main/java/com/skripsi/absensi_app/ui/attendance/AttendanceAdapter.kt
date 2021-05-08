04ace1ee37f1647503db4b4dd4553150b0636774
imufiid <imufiid98@gmail.com>
2021-05-08 10:23:37 +0700

    fix : holder data

--- app/src/main/java/com/skripsi/absensi_app/ui/attendance/AttendanceAdapter.kt
+++ app/src/main/java/com/skripsi/absensi_app/ui/attendance/AttendanceAdapter.kt
@@ -3,10 +3,13 @@ package com.skripsi.absensi_app.ui.attendance
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.skripsi.absensi_app.R
 import com.skripsi.absensi_app.data.source.local.entity.AttendanceEntity
 import com.skripsi.absensi_app.databinding.ItemAttendanceBinding
 
-class AttendanceAdapter : RecyclerView.Adapter<AttendanceAdapter.ViewHolder>() {
+class AttendanceAdapter(
+    private val onClick: (AttendanceEntity) -> Unit
+) : RecyclerView.Adapter<AttendanceAdapter.ViewHolder>() {
 
     val data = ArrayList<AttendanceEntity>()
     override fun onCreateViewHolder(
@@ -19,20 +22,43 @@ class AttendanceAdapter : RecyclerView.Adapter<AttendanceAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        data[position].let {
-            holder.bind(it)
-        }
+        holder.bind(data[position])
     }
 
     override fun getItemCount(): Int = data.size
 
-    class ViewHolder(private val binding: ItemAttendanceBinding) :
+    inner class ViewHolder(private val binding: ItemAttendanceBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(attendance: AttendanceEntity) {
             with(binding) {
+                itemView.setOnClickListener {
+                    onClick(attendance)
+                }
+                // attendance time
                 timeIn.text = if (attendance.timeComes == "0") "00:00:00" else attendance.timeComes
-                timeOut.text = if (attendance.timeGohome == "0") "00:00:00" else attendance.timeGohome
+                timeOut.text =
+                    if (attendance.timeGohome == "0") "00:00:00" else attendance.timeGohome
                 datetime.text = attendance.date
+
+                // attendance type
+                attendanceType.text = when (attendance.attendanceType?.toInt()) {
+                    1 -> "Masuk"
+                    2 -> "Ijin"
+                    3 -> "Sakit"
+                    else -> "-"
+                }
+
+                // validation
+                when (attendance.isValidate?.toInt()) {
+                    1 -> {
+                        isValidation.text = "Sudah Divalidasi"
+                        isValidation.setBackgroundResource(R.drawable.bg_text_validation)
+                    }
+                    0 -> {
+                        isValidation.text = "Belum Divalidasi"
+                        isValidation.setBackgroundResource(R.drawable.bg_text_not_validation)
+                    }
+                }
             }
         }
     }
