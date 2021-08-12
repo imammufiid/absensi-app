350ae5c2473d3441ce23a80bf76a53ab923060d9
imufiid <imufiid98@gmail.com>
2021-08-12 20:26:42 +0700

    setup helper to day of week indonesian

---
+++ app/src/main/java/com/skripsi/absensi_app/utils/helper/DayIndonesian.kt
@@ -0,0 +1,14 @@
+package com.skripsi.absensi_app.utils.helper
+
+object DayIndonesian {
+
+    val dayOfWeek = mapOf<String, String>(
+        "Sunday" to "Minggu",
+        "Monday" to "Senin",
+        "Tuesday" to "Selasa",
+        "Wednesday" to "Rabu",
+        "Thursday" to "Kamis",
+        "Friday" to "Jumat",
+        "Saturday" to "Sabtu",
+    )
+}
